6fdf97e48d080183cfa827029995e3766e3f4336
hnhu2670 <2051050327nhu@ou.edu.vn>
2023-10-06 16:21:11 +0700

    change

--- Clinnic/target/generated-sources/annotations/com/owen/pojo/Rating_.java
+++ Clinnic/target/generated-sources/annotations/com/owen/pojo/Rating_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2023-10-05T13:43:31")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2023-10-06T12:16:08")
 @StaticMetamodel(Rating.class)
 public class Rating_ { 
 
--- Clinnic/target/generated-sources/annotations/com/owen/pojo/Role_.java
+++ Clinnic/target/generated-sources/annotations/com/owen/pojo/Role_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2023-10-05T13:43:31")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2023-10-06T12:16:08")
 @StaticMetamodel(Role.class)
 public class Role_ { 
 
